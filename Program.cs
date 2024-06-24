6980c6f9f7f4ecf465803e05d32b3022280aaf91
miri <mt0548553246@gmail.com>
2024-06-24 23:53:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WordDocReader reader = WordDocReader.GetInstance();
+            reader.filePath = "\"D:\\users\\user1\\Downloads\\1.docx\"";
+            reader.openedDate = DateTime.Now;
+
+            reader.OpenFile();
+            reader.BackupFile();
+            
+        }
+    }
+}
---
+++ WordDocReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class WordDocReader 
+    {
+        private static WordDocReader instance;
+        public string filePath { get; set; }
+        public DateTime openedDate { get; set; }
+
+        private WordDocReader() { }
+
+        public static WordDocReader GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new WordDocReader();
+            }
+            return instance;
+        }
+
+        public void OpenFile()
+        {
+            // פונקציה הפותחת קובץ ומציגה אותו על המסך
+            Console.WriteLine("Opening file: " + filePath);
+        }
+
+        public void BackupFile()
+        {
+            // פונקציית גיבוי - היוצרת עותק של הקובץ
+            Console.WriteLine("Creating backup of file: " + filePath);
+        }
+
+       
+    }
+}
